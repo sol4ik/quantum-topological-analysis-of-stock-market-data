f36dc1101931ab656791536d9bbe1181d0efe017
solka13Lenio <solomialenio13@gmail.com>
2020-06-14 15:46:03 +0300

    fixed preprocessing

--- modules/preprocessing.py
+++ modules/preprocessing.py
@@ -16,17 +16,23 @@ def norm(vect):
 
 def to_angles(vect):
     """
-    Represent point from surface of Bloch sphere within two angles.
-    (x, y, z) = (sin a cos b, sin a sin b, cos b)
+    Represent point from surface of Bloch sphere within three angles.
+    ! In this case input vector is 2-dimensional, but in general case - any power of 2
+
+    |s> = a |0> + b |1>
+    |s> = cos (a` / 2) |0> + e^(i * phi) sin (a` / 2) |1>
+    Since a and b are real numbers, phi = 0.
+    Thus a` = 2 * arccos(a) and b = 2 * arcsin(a)
+
     Represent each angle within pi.
-    :param vect: inout point
+    :param vect: input point
     :return: 2-element tuple - values of angles (a, b)
     """
-    b = arccos(vect[2])
-    a = arcsin(vect[0] / b)
+    a = 2 * arccos(vect[0])
+    b = 2 * arcsin(vect[1])
     pi_a = "pi*" + str(round(a / pi, 3))
     pi_b = "pi*" + str(round(b / pi, 3))
-    return pi_a, pi_b
+    return pi_a, pi_b, "pi*0"
 
 
 def preprocess(path):
@@ -50,27 +56,28 @@ def preprocess(path):
 
     # data cloud - takens' embedding
     data_cloud = list()
-    embedding_dim = 3
+    embedding_dim = 2  # ! power of 2
     embedding_time_delay = 2
     for i in range(len(resampled_data) - embedding_dim * embedding_time_delay):
         to_append = [resampled_data[i + embedding_time_delay * j] for j in range(embedding_dim)]
         data_cloud.append(tuple(to_append))
 
-    # normalize data - points on Bloch sphere
+    # construct quantum state for each point
+    q_coefs = list()
     for i in range(len(data_cloud)):
-        p_norm = norm(data_cloud[i])
-        data_cloud[i] = tuple([data_cloud[i][j] / p_norm for j in range(embedding_dim)])
+        cur_norm = norm(data_cloud[i])
+        q_coefs.append(tuple([data_cloud[i][j] / cur_norm for j in range(embedding_dim)]))
 
     # to angles - for U3 gate
     angles = list()
-    for vect in data_cloud:
+    for vect in q_coefs:
         angles.append(to_angles(vect))
 
     # write circuit configurations to .csv file
     with open("results/circuit_conf.csv", 'w') as config_file:
-        print("a,b", file=config_file)
+        print("a,b,phi", file=config_file)
         for vect in angles:
-            print("{},{}".format(vect[0], vect[1]), file=config_file)
+            print("{},{},{}".format(vect[0], vect[1], vect[2]), file=config_file)
 
 
 if __name__ == "__main__":
